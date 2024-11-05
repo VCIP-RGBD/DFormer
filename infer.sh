4792d50856e1452e4218dcd370e8d2a0b7e15f10
Bo-Wen Yin <52337138+yinbow@users.noreply.github.com>
2024-11-06 01:49:48 +0800

    Update infer.sh

--- infer.sh
+++ infer.sh
@@ -9,6 +9,7 @@ MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch \
+    --use-env \
     --nnodes=$NNODES \
     --node_rank=$NODE_RANK \
     --master_addr=$MASTER_ADDR \
